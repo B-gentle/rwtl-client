1fb97d7de95a59f1326afd682a2ec8fbb05e5bde
beetech <brighteyo16@gmail.com>
2023-07-02 19:30:05 +0100

    settling a mess

--- src/pages/adminPages/ViewTransaction.js
+++ src/pages/adminPages/ViewTransaction.js
@@ -1,39 +1,73 @@
 import React, { useState } from 'react';
-import { Form, Button, Input, Select, message } from 'antd';
+import { Form, Button, Input, Select, message, Table } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { SET_LOADING, selectLoading, SET_SUCCESS, SET_ERROR } from '../../redux/features/processingStates/processStatesSlice'
 import BackArrowHeading from '../../components/BackArrowHeading';
 import { ViewUserTransactions } from '../../services/adminCalls';
 
+
 const ViewTransaction = () => {
 
-  const [userTransaction, setUserTransaction] = useState(null)
+  const [userTransaction, setUserTransaction] = useState(null);
   const dispatch = useDispatch()
   const loading = useSelector(selectLoading)
+
   const onFinish = async (values) => {
-try {
-  dispatch(SET_LOADING())
-  const response = await ViewUserTransactions({username: values.username, transactionType: values.transactionType})
-  if (response.status === 200){
-    dispatch(SET_SUCCESS())
-    setUserTransaction(response.data.data)
-  }else{
-    const message =
+    try {
+      dispatch(SET_LOADING())
+      const response = await ViewUserTransactions({ username: values.username, transactionType: values.transactionType })
+      if (response.status === 200) {
+        dispatch(SET_SUCCESS())
+        setUserTransaction(response.data.data)
+      } else {
+        const message =
           (response.data && response.data.message) || (response.response && response.response.data && response.response.data.message) ||
           response.message ||
           response.toString();
         throw new Error(message)
+      }
+      console.log(response)
+    } catch (error) {
+      dispatch(SET_ERROR())
+      message.error(error.message)
+    }
   }
-  console.log(response)
-} catch (error) {
-  dispatch(SET_ERROR())
-  message.error(error.message)
-}
-  }
+
+
+  const columns = [
+    {
+      title: 'Network',
+      dataIndex: 'network',
+      key: 'network',
+    },
+
+    {
+      title: 'Sender',
+      dataIndex: 'sender',
+      key: 'sender',
+    },
+    {
+      title: 'Receiver',
+      dataIndex: 'recipient',
+      key: 'recipient',
+    },
+    {
+      title: 'Transaction Type',
+      dataIndex: 'transactionType',
+      key: 'transactionType',
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+    },
+  ];
+
   return (
     <div className='p-[1rem]'>
       <BackArrowHeading title="View User Transaction" link="admin" />
-      <Form
+     { !userTransaction ? 
+     (<Form
         className='mt-[1rem]'
         onFinish={onFinish}
       >
@@ -107,7 +141,9 @@ try {
             Submit
           </Button>
         </Form.Item>
-      </Form>
+      </Form>) : (
+        <Table dataSource={userTransaction} columns={columns} />
+      )}
     </div>
   )
 }
