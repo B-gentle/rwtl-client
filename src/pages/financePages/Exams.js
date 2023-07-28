cea9ff086b210f028908f3153accd508880536a8
beetech <brighteyo16@gmail.com>
2023-07-28 05:59:38 +0100

    added commission history

--- src/pages/financePages/Exams.js
+++ src/pages/financePages/Exams.js
@@ -4,12 +4,12 @@ import BackArrowHeading from '../../components/BackArrowHeading'
 
 const Exams = () => {
   return (
-    <div className='buy-airtime h-full'>
+    <div className='buy-airtime'>
       <BackArrowHeading title="Exam Payment" link="more" />
-      <h2 className='mb-[1.5rem] text-[#33a533]'>Select Examination Type</h2>
+      <h2 className='mb-[1.5rem] text-[#cba762]'>Select Examination Type</h2>
       <section className='flex flex-col gap-[1.5rem] items-center'>
-        <span><Link to='/waec' className='text-[#33aa33] no-underline font-[600]'>WAEC e-PIN</Link></span>
-        <span><Link to='/jamb' className='text-[#33aa33] no-underline font-[600]'>JAMB e-PIN</Link></span>
+        <span><Link to='/waec' className='text-[#7c7c5c] no-underline font-[600]'>WAEC e-PIN</Link></span>
+        <span><Link to='/jamb' className='text-[#7c7c5c] no-underline font-[600]'>JAMB e-PIN</Link></span>
       </section>
     </div>
   )
